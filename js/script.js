b0329b68d450e748f6303b97aba4111eaf51cbe8
itsuko-nagai <itsukonagai20@gmail.com>
2025-10-16 16:13:46 +0530

    Initial push - basic code

---
+++ js/script.js
@@ -0,0 +1,90 @@
+const canvas = document.getElementById('particle-canvas');
+const ctx = canvas.getContext('2d');
+
+let particles = [];
+const particleCount = 100;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+class Particle {
+    constructor(x, y, size, color, speedX, speedY) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+
+    update() {
+        if (this.x < 0 || this.x > canvas.width) {
+            this.speedX = -this.speedX;
+        }
+        if (this.y < 0 || this.y > canvas.height) {
+            this.speedY = -this.speedY;
+        }
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function init() {
+    particles = [];
+    for (let i = 0; i < particleCount; i++) {
+        const size = Math.random() * 2 + 1;
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        const speedX = (Math.random() - 0.5) * 0.5;
+        const speedY = (Math.random() - 0.5) * 0.5;
+        const color = 'rgba(255, 140, 0, 0.5)';
+        particles.push(new Particle(x, y, size, color, speedX, speedY));
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].draw();
+    }
+    connect();
+    requestAnimationFrame(animate);
+}
+
+function connect() {
+    let opacityValue = 1;
+    for (let a = 0; a < particles.length; a++) {
+        for (let b = a; b < particles.length; b++) {
+            const distance = Math.sqrt(
+                (particles[a].x - particles[b].x) * (particles[a].x - particles[b].x) +
+                (particles[a].y - particles[b].y) * (particles[a].y - particles[b].y)
+            );
+
+            if (distance < 100) {
+                opacityValue = 1 - (distance / 100);
+                ctx.strokeStyle = `rgba(255, 140, 0, ${opacityValue})`;
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.moveTo(particles[a].x, particles[a].y);
+                ctx.lineTo(particles[b].x, particles[b].y);
+                ctx.stroke();
+            }
+        }
+    }
+}
+
+init();
+animate();
